d4a5e71048cf228bccdc1f79e60548c13fae2880
jaredshue <74428951+jaredshue@users.noreply.github.com>
2020-12-01 16:07:34 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // ...
+    document.getElementById("navbar-links-home").addEventListener("click", function() {
+        this.setAttribute("class", "navbar-link active");
+        document.getElementById("navbar-links-login").setAttribute("class", "navbar-link");
+        document.getElementById("login-view").setAttribute("class", "login-view");
+        document.getElementById("movies-view").setAttribute("class", "movies-view active");
+    });
+
+    document.getElementById("navbar-links-login").addEventListener("click", function() {
+        this.setAttribute("class", "navbar-link active");
+        document.getElementById("navbar-links-home").setAttribute("class", "navbar-link");
+        document.getElementById("movies-view").setAttribute("class", "movies-view");
+        document.getElementById("login-view").setAttribute("class", "login-view active");
+    });
 });
